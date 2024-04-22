134870a2031e16d13fbd40b40cd12905e73ae6f6
たかはしだいき <k022g0073@g.ne.jp>
2024-04-22 15:24:19 +0900

    knsei

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -7,7 +7,8 @@ public class PlayerScript : MonoBehaviour
 
     public Rigidbody rb;
 
-    int moveSpeed;
+    int moveSpeed = 3;
+
 
 
     // Start is called before the first frame update
@@ -21,7 +22,8 @@ public class PlayerScript : MonoBehaviour
     {
         Vector3 v = rb.velocity;
 
-        if(Input.GetKey(KeyCode.RightArrow))
+
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             v.x = moveSpeed;
         }
@@ -29,6 +31,31 @@ public class PlayerScript : MonoBehaviour
         {
             v.x = 0;
         }
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            v.x -= moveSpeed;
+        }
+        else 
+        {
+            v.x = 0;
+        }
+
+        rb.velocity = v;
+
+    }
+
+    void Update()
+    {
+        Vector3 v = rb.velocity;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            v.y = moveSpeed;
+        }
+
         rb.velocity = v;
     }
+
 }
